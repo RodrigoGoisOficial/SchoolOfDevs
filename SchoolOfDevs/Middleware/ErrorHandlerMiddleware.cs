a11134a257612f0986a34ad06d56e8e5979372d4
Rodrigo Gois <rodrigojosedegois@gmail.com>
2023-02-07 21:38:37 -0300

    Creating the Middleware

---
+++ SchoolOfDevs/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using SchoolOfDevs.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace SchoolOfDevs.Middleware
+{
+    public class ErrorHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async void Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception error)
+            {
+                var response = context.Response;
+                response.ContentType= "application/json";
+                response.StatusCode = error switch
+                {
+                    BadRequestException => (int)HttpStatusCode.BadRequest, // custom application eror
+                    KeyNotFoundException => (int)HttpStatusCode.NotFound, // not faund error    
+                    ForbiddenException => (int)HttpStatusCode.Forbidden, // unhandled error
+                    _ => (int)HttpStatusCode.InternalServerError,
+                };
+                
+                var result = JsonSerializer.Serialize(new { message = error?.Message });
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
