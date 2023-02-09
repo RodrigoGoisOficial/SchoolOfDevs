f57137a25298a308c00aa5641a5803f5422a3b3a
Rodrigo Gois <rodrigojosedegois@gmail.com>
2023-02-08 23:27:27 -0300

    Data Transfer Object (DTO)

---
+++ SchoolOfDevs/Dto/User/UserRequest.cs
@@ -0,0 +1,16 @@
+﻿using SchoolOfDevs.Enuns;
+
+namespace SchoolOfDevs.Dto.User
+{
+    public class UserRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public TypeUser TypeUser { get; set; }
+        public int[] CoursesStudingIds { get; set; }
+    }
+}
---
+++ SchoolOfDevs/Dto/User/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using SchoolOfDevs.Entities;
+using SchoolOfDevs.Enuns;
+
+namespace SchoolOfDevs.Dto.User
+{
+    public class UserResponse
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string UserName { get; set; }
+        public TypeUser TypeUser { get; set; }
+        public List<Course> CoursesStuding { get; set; } // TypeUser == Student
+        public List<Course> CoursesTeaching { get; set; } // TypeUser == Teacher
+    }
+}
