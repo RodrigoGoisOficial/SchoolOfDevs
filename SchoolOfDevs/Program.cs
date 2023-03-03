7e852b1b6cbbf61f60947d1ebe75097977c6f64f
Rodrigo Gois <rodrigojosedegois@gmail.com>
2023-03-02 22:34:48 -0300

    JwtService

--- SchoolOfDevs/Program.cs
+++ SchoolOfDevs/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
+builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<INoteService, NoteService>();
 builder.Services.AddScoped<ICourseService, CourseService>();
